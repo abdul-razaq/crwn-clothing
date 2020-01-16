bf9ebb24602b7a3cb60bdb3966988cc94c314375
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-01-16 23:19:54 +0100

    Added google fonts link

---
+++ src/components/sign-in/sign-in.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import './sign-in.scss';
+
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  render() {
+    return(
+      <div className="sign-in">
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form>
+          <input type="email" name="email" value/>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
