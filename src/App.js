6d00c4b85f4eee573b0d627b29bf92d49d2acd33
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-01-16 10:16:09 +0100

    Finished styling the header component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage';
 import ShopPage from './pages/shop/shop';
-
+import Header from './components/header/header';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/shop" component={ShopPage} />
-    </Switch>
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
+    </div>
   );
 }
 
