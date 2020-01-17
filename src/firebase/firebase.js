6899c9759abb7ae0383f8670e8dd9e4be6ff9ddc
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-01-17 22:37:09 +0100

    Installed firebase library and implemented basic firebase config

---
+++ src/firebase/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+// Setup config object
+const config = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+};
