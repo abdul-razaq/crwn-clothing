cfee9115cedb4319a04a93efb901e66302b9afcc
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-01-18 08:09:33 +0100

    Finsihed styling the signIn component

--- src/components/sign-in/sign-in.js
+++ src/components/sign-in/sign-in.js
@@ -51,10 +51,12 @@ class SignIn extends React.Component {
             handleChange={this.handleChange}
           />
 
-          <CustomButton type="submit">Sign In</CustomButton>
-          <CustomButton onClick={signInWithGoogle}>
-            Sign in with Google
-          </CustomButton>
+          <div className="buttons">
+            <CustomButton type="submit">Sign In</CustomButton>
+            <CustomButton isGoogleSignIn onClick={signInWithGoogle}>
+              Sign in with Google
+            </CustomButton>
+          </div>
         </form>
       </div>
     );
