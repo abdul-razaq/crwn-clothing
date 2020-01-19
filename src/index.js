92b20facaedb052792892103f3df1c02079acf5b
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-01-19 12:39:03 +0100

    Added store directory for action object and Reducers

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from './store/reducers';
 
 import './index.css';
 import App from './App';
 
+const store = createStore(rootReducer);
+
 const rootComponent = (
-  <Router>
-    <App />
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
 );
 
 ReactDOM.render(rootComponent, document.getElementById('root'));
