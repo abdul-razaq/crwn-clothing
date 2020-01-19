cbfda1ed566d442c634a83f02788f6fff95711de
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-01-19 21:28:51 +0100

    Added cartItem utils logic

--- src/store/reducers/cartReducer.js
+++ src/store/reducers/cartReducer.js
@@ -1,4 +1,6 @@
 import { TOGGLE_CART_HIDDEN, ADD_ITEM } from '../actions/types';
+import addItemToCart from '../../utils/cartUtils';
+import { addItem } from '../actions';
 
 const INITIAL_STATE = {
   hidden: true,
@@ -15,8 +17,8 @@ export const cartReducer = (state = INITIAL_STATE, action) => {
     case ADD_ITEM:
       return {
         ...state,
-        cartItems: [...state.cartItems, action.payload]
-      }
+        cartItems: addItemToCart(state.cartItems, action.payload),
+      };
     default:
       return state;
   }
--- src/utils/cartUtils.js
+++ src/utils/cartUtils.js
@@ -1,1 +1,14 @@
-export const addItemToCart
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  // Check inside of the existing cart items if the cart already exists
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToAdd.id
+  );
+  if (existingCartItem) {
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
