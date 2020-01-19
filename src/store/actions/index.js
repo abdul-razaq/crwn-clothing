4039ed68d496902350d5dca09fa71ec4cf1a55d3
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-01-19 13:00:28 +0100

    Added the SET_CURRENT_USER action creator

---
+++ src/store/actions/index.js
@@ -0,0 +1,9 @@
+// Action creators live here
+import { SET_CURRENT_USER } from './types';
+
+export const setCurrentUser = user => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: user,
+  };
+};
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import logger from 'redux-logger';
+
 import rootReducer from './reducers';
 
 const middlewares = [logger];
+
 export default createStore(rootReducer, applyMiddleware(...middlewares));
