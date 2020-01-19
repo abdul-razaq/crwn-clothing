60d20d7594ebd54fbe055c4ee535b73c0f2e3bca
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-01-19 18:35:29 +0100

    Added cartIcon and cartDropdown components to our application, also implemented cart reducer

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,9 +6,10 @@ import { auth } from '../../firebase/firebase';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import CartIcon from '../cart-icon/cart-icon';
+import CartDropdown from '../cart-dropdown/cart-dropdown';
 import './header.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -31,11 +32,13 @@ const Header = ({ currentUser }) => (
       )}
       <CartIcon />
     </div>
+    {hidden ? <CartDropdown /> : null}
   </div>
 );
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser,
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
 });
 
 export default connect(mapStateToProps)(Header);
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,5 +1,5 @@
 // Action creators live here
-import { SET_CURRENT_USER } from './types';
+import { SET_CURRENT_USER, TOGGLE_CART_HIDDEN } from './types';
 
 export const setCurrentUser = user => {
   return {
@@ -7,3 +7,5 @@ export const setCurrentUser = user => {
     payload: user,
   };
 };
+
+export const toggleCartHidden = () => ({ type: TOGGLE_CART_HIDDEN });
