384b5e0c69219ded07d15d545c66ae54343b19f3
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-01-20 02:10:32 +0100

    Fixed some bugs

--- src/components/cart-icon/cart-icon.js
+++ src/components/cart-icon/cart-icon.js
@@ -5,16 +5,16 @@ import { ReactComponent as ShoppingIcon } from '../../assets/shopping.svg';
 import './cart-icon.scss';
 import { toggleCartHidden } from '../../store/actions';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
 
 const mapStateToProps = ({ cart: { cartItems } }) => ({
   itemCount: cartItems.reduce(
-    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem,
+    (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity,
     0
   ),
 });
