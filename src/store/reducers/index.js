366def6c86a4497861b690e99cd7b38268053375
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-01-20 11:57:51 +0100

    Added redux-persist library to the application to persist cart items to the session storage on page refresh

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,8 +1,20 @@
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import userReducer from './userReducer';
 import cartReducer from './cartReducer';
 
-export default combineReducers({
+// Configurations we want redux-persist to use
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['cart'],
+};
+
+const rootReducer = combineReducers({
   user: userReducer,
-  cart: cartReducer
+  cart: cartReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
