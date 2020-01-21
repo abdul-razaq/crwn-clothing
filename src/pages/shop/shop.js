83bbae1a83c0e0bfb793499a975df72862f998c6
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-01-21 23:17:18 +0100

    Added collections-overview component logic

--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -1,22 +1,14 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview';
-import { selectCollections } from '../../store/reducers/shop/shop_selector';
+import CollectionsOverview from '../../components/collections-overview/collections-overview';
 
 const ShopPage = ({ collections }) => {
   return (
     <div className="shop-page">
-      {collections.map(({ id, ...otherCollectionProps }) => (
-        <CollectionPreview key={id} {...otherCollectionProps} />
-      ))}
+      <CollectionsOverview />
     </div>
   );
 };
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
-})
 
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
