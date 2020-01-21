8af040f3f13b9d744e1332f76e399df3490c4965
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-01-21 23:50:22 +0100

    Renamed category component to collection component

---
+++ src/pages/collection/collection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import CollectionItem from '../../components/collection-item/collection-item';
+import './collection.scss';
+
+const CollectionPage = ({ match }) => (
+  <div className="collection-page">
+    <h2>CollectionPage</h2>
+  </div>
+);
+
+export default CollectionPage;
--- src/pages/shop/shop.js
+++ src/pages/shop/shop.js
@@ -2,14 +2,19 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview';
+import CollectionPage from '../collection/collection';
 
 const ShopPage = ({ match }) => {
   return (
     <div className="shop-page">
       <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route
+        exact
+        path={`${match.path}/:collectionId`}
+        component={CollectionPage}
+      />
     </div>
   );
 };
-
 
 export default ShopPage;
