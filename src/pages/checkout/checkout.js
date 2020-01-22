26ec998e205b02778babe72798b2f1fa918c3600
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2020-01-22 09:59:57 +0100

    Added some styling

--- src/pages/checkout/checkout.js
+++ src/pages/checkout/checkout.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
+
 import CheckoutItem from '../../components/checkout-item/checkout-item';
 import './checkout.scss';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button';
 
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
@@ -25,9 +27,13 @@ const CheckoutPage = ({ cartItems, total }) => (
     {cartItems.map(cartItem => (
       <CheckoutItem key={cartItem.id} cartItem={cartItem} />
     ))}
-    <div className="total">
-      <span>TOTAL: ${total}</span>
+    <div className="total">TOTAL: ${total}</div>
+    <div className="test-warning">
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
     </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
